f1ecfdc4602d8d00de1093507a1785d944fd7e24
timcarey <timcarey1989@gmail.com>
2023-05-21 00:27:47 -0400

    rework tailwind.config.js

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,24 +1,18 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    content: [
+        './app/views/**/*.{erb,haml,html,slim}',
+        './public/*.html',
+        './app/helpers/**/*.rb',
+        './app/assets/stylesheets/**/*.css',
+        './app/javascript/**/*.js'
+    ],
+    theme: {
+        listStyleType: {
+            decimal: 'decimal',
+            disc: "disc"
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-    require("daisyui"),
-  ]
+
+    plugins: [
+        require("daisyui")],
 }
