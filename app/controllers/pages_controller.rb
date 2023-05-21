08c4dd90e02dfb6f4f6e0a3b039e21bc684a6b56
timcarey <timcarey1989@gmail.com>
2023-05-21 00:28:08 -0400

    add routes to pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,44 @@
 class PagesController < ApplicationController
+
+  def adopting
+  end
+
+  def ourdogs
+  end
+
+  def ourcats
+  end
+
+  def foster
+  end
+
+  def aboutus
+  end
+
+  def contactus
+  end
+
+  def faq
+  end
+
+  def donate
+  end
+
+  def volunteer
+  end
+
+  def events
+  end
+
+  def fundraising
+  end
+
+  def wishlist
+  end
+
+  def wagswiki
+  end
+
+  def amazonsmile
+  end
 end
