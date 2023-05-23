54519d974bbf3b504da7b6ad2d1f3b53620420f2
timcarey <timcarey1989@gmail.com>
2023-05-23 14:59:55 -0400

    add assertion for card content

--- test/components/card_component_test.rb
+++ test/components/card_component_test.rb
@@ -2,8 +2,11 @@ require "test_helper"
 
 class CardComponentTest < ViewComponent::TestCase
   def test_render_component
-    render_inline(CardComponent.new(title: "my title"))
+    render_inline(CardComponent.new(title: "my title")) do
+      "Some content!"
+    end
 
     assert_selector "h2[class='card-title']", text: "my title"
+    assert_text "Some content!"
   end
 end
