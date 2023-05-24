944083eccf552f4acc4c6e1443cdbdba2e3c4284
timcarey <timcarey1989@gmail.com>
2023-05-23 23:27:56 -0400

    letter_opener gem(and config gem) + set up mailer for contact form + build contact us page(still WIP)

---
+++ app/controllers/contact_controller.rb
@@ -0,0 +1,12 @@
+class ContactController < ApplicationController
+
+  def send_email
+    name = params[:name]
+    email = params[:email]
+    message = params[:message]
+
+    ContactMailer.send_email(name, email, message).deliver_now
+
+    redirect_to contact_us_path, notice: 'Email sent successfully!'
+  end
+end
