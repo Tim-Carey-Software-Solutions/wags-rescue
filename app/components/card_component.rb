9a46a66072c212c1c7a24986d13ffc1d2152961c
timcarey <timcarey1989@gmail.com>
2023-06-01 15:19:30 -0400

    set tailwind config to read component .rb files + set default timezone + add option to add more card container classes to component

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -1,7 +1,8 @@
 class CardComponent < ViewComponent::Base
 
-  def initialize(title:, extra_body_classes: nil)
+  def initialize(title:, extra_body_classes: nil, extra_container_classes: "sm:w-96")
     @title = title
     @extra_body_classes = extra_body_classes
+    @extra_container_classes = extra_container_classes
   end
 end
