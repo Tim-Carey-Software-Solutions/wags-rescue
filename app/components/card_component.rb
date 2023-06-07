74396304766e109908e68c2cda64760a06cdff38
timcarey <timcarey1989@gmail.com>
2023-06-06 21:24:01 -0400

    add images to card component

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -1,7 +1,10 @@
 class CardComponent < ViewComponent::Base
 
-  def initialize(title:, extra_body_classes: nil, extra_container_classes: "sm:w-96")
+  def initialize(title:, extra_title_classes: nil, extra_body_classes: nil, extra_container_classes: "sm:w-96", image_src: nil, image_alt: nil)
     @title = title
+    @image_src = image_src
+    @image_alt = image_alt
+    @extra_title_classes = extra_title_classes
     @extra_body_classes = extra_body_classes
     @extra_container_classes = extra_container_classes
   end
