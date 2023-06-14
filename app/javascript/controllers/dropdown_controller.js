bdcbb3fda5efb1003e4dcd5c8b8c70f18cc313d7
timcarey <timcarey1989@gmail.com>
2023-06-14 07:50:12 -0400

    make dropdown javascript more re-usable

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,31 +2,17 @@ import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
     connect() {
-        const aboutDropdown = document.getElementById("about-dropdown");
-        const fosterDropdown = document.getElementById("foster-dropdown");
-        const adoptDropdown = document.getElementById("adopt-dropdown");
+        const details = document.querySelectorAll("details");
 
-        adoptDropdown.addEventListener('toggle', () => {
-            if (adoptDropdown.open && fosterDropdown.open) {
-                fosterDropdown.removeAttribute('open')
-            } else if (adoptDropdown.open && aboutDropdown.open) {
-                aboutDropdown.removeAttribute('open')
-            }
-        })
-
-        aboutDropdown.addEventListener('toggle', () => {
-            if (aboutDropdown.open && fosterDropdown.open) {
-                fosterDropdown.removeAttribute('open')
-            } else if (aboutDropdown.open && adoptDropdown.open) {
-                adoptDropdown.removeAttribute('open')
-            }
-        })
-
-        fosterDropdown.addEventListener('toggle', () => {
-            if (fosterDropdown.open && aboutDropdown.open) {
-                aboutDropdown.removeAttribute('open')
-            }
-        })
+        details.forEach((targetDetail) => {
+            targetDetail.addEventListener("click", () => {
+                details.forEach((detail) => {
+                    if (detail !== targetDetail) {
+                        detail.removeAttribute("open");
+                    }
+                });
+            });
+        });
 
     }
 }
