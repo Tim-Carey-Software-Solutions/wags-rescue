797fa9003a4156098180879b4aaa5bd11c95e4ff
timcarey <timcarey1989@gmail.com>
2023-06-20 16:21:58 -0400

    formatted user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,21 +18,18 @@
 #  name                   :string
 #
 class User < ApplicationRecord
-  
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[google_oauth2]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable,
+         omniauth_providers: %i[google_oauth2]
 
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
 
-    unless user
-        user = User.create(name: data['name'],
-           email: data['email'],
-           password: Devise.friendly_token[0,20],
-           provider: access_token.provider,
-           uid: access_token.uid
-        )
-    end
+    user ||= User.create(name: data['name'],
+                         email: data['email'],
+                         password: Devise.friendly_token[0, 20],
+                         provider: access_token.provider,
+                         uid: access_token.uid)
     user
-end
+  end
 end
