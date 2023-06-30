88bcb0b86d031e8ce67e41e422a23d90f7205e46
timcarey <timcarey1989@gmail.com>
2023-06-29 23:19:49 -0400

    set up foster applications

---
+++ app/controllers/foster_applications_controller.rb
@@ -0,0 +1,2 @@
+class FosterApplicationsController < ApplicationController
+end
---
+++ test/controllers/foster_applications_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FosterApplicationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
