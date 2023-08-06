a5919b0a97c19d36fd9b6f3312445b5f2d417469
timcarey <timcarey1989@gmail.com>
2023-08-06 17:13:26 -0400

    fix adminstrate to have name for events

--- app/dashboards/event_dashboard.rb
+++ app/dashboards/event_dashboard.rb
@@ -11,6 +11,7 @@ class EventDashboard < Administrate::BaseDashboard
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     address: Field::String,
+    name: Field::String,
     end: Field::Time,
     location: Field::String,
     start: Field::Time,
@@ -26,6 +27,7 @@ class EventDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
     location
+    name
     address
     start
     end
@@ -36,6 +38,7 @@ class EventDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     location
+    name
     address
     start
     end
@@ -47,6 +50,7 @@ class EventDashboard < Administrate::BaseDashboard
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
     location
+    name
     address
     start
     end
